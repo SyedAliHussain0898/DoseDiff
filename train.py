8e81edccde4fd552f0ee5389af51d22ef8f2c9fe
SyedAliHussain0898 <143449551+SyedAliHussain0898@users.noreply.github.com>
2025-02-24 21:45:38 -0500

    Update train.py

--- train.py
+++ train.py
@@ -15,12 +15,12 @@ from torchvision.utils import make_grid
 from torch.optim.lr_scheduler import MultiStepLR
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--bs', type=int, default=32, help='batch size')
+parser.add_argument('--bs', type=int, default=64, help='batch size')
 parser.add_argument('--T', type=int, default=1000, help='T')
 parser.add_argument('--epoch', type=int, default=2, help='all_epochs')
 args = parser.parse_args()
 
-device = torch.device("cpu")
+device = torch.device("cuda")
 
 train_bs = args.bs
 val_bs = args.bs
